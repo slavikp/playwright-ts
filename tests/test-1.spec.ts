37b986ce02dbc5995fbf3d8281aff82d876fa500
slavikp <slavik.pashanin@gmail.com>
2024-06-13 22:58:16 -0500

    add sanity test + remove default playwright tests

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="add-to-cart-test\\.allthethings\\(\\)-t-shirt-\\(red\\)"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('.checkout_info').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('slava');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('pashanin');
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill('r3p 1g6');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.locator('[data-test="secondary-header"]').click();
+  await page.locator('[data-test="reset-sidebar-link"]').click();
+  await page.locator('[data-test="about-sidebar-link"]').click();
+  await page.locator('.MuiBox-root > .MuiContainer-root > div > div').first().click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+});
