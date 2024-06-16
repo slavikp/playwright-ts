da579bcdf98fc3bf3232fda4fb6d23a47d342909
slavikp <slavik.pashanin@gmail.com>
2024-06-16 00:16:52 -0500

    adding more basic tests

---
+++ tests/basic.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "@playwright/test";
+
+test("basic test", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+  await page.waitForTimeout(1000);
+
+  const userNameField = await page.locator("#user-name").type("standard_user", { delay: 50 });
+  const passwordNameField = await page.locator('[data-test="password"]').type("secret_sauce", {delay: 50});
+  const loginButton = await page.locator("#login-button").click();
+    
+  await page.waitForTimeout(5000);
+});
