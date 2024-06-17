2d475257e2d1e2ad08233c8a8641ea3728ea6a4c
slavikp <slavik.pashanin@gmail.com>
2024-06-17 08:03:02 -0500

    add bsic tests

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -7,6 +7,7 @@ test("basic test", async ({ page }) => {
   const userNameField = await page.locator("#user-name").type("standard_user", { delay: 50 });
   const passwordNameField = await page.locator('[data-test="password"]').type("secret_sauce", {delay: 50});
   const loginButton = await page.locator("#login-button").click();
-    
+
   await page.waitForTimeout(5000);
+  page.screenshot({fullPage: true});
 });
